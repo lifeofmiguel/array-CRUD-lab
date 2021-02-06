0dc73a6dfaa1e1a4a3450fee597700c520c218c8
Miguel Angel Morales <mmorales21500@live.com>
2021-02-06 15:39:55 -0500

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,34 @@
 def create_an_empty_array
-  
+    []
 end
 
 def create_an_array
-  
+  muscle_cars = ["Charger","Challenger","Mustang","Camaro"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << "arrays!"
 end
-
 def add_element_to_start_of_array(array, element)
-  
-end
+array.unshift("wow")
+ end
 
 def remove_element_from_end_of_array(array)
-  
+   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
